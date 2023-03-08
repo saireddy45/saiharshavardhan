beea02f1427ecffacb7b93dd28206745b7bb3575
sai <harshavardhansai7990@gmail.com>
2023-03-08 19:58:45 +0530

    second commit

---
+++ src.py
@@ -0,0 +1,7 @@
+git commands
+-------------
+git init to create a . git folder in the in the . git folder 
+git add. 
+git coomit -m " first commit"
+git push origin
+in git we have five areas of git
